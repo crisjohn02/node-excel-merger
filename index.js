05d442afb6178b3e7b16e86d13e9d261e05610bd
Cris <crisjohnreytarpin@gmail.com>
2022-11-29 16:45:31 +0000

    updates

--- index.js
+++ index.js
@@ -4,31 +4,15 @@ const json2csv = require('json2csv');
 const parser = require('./utils/parser');
 const _ = require('lodash');
 const XLSX = require('xlsx');
-// XLSX.set_fs(fs);
 
-// if(!fs.existsSync(path.join(__dirname,'csv')))
-//     fs.mkdirSync(path.join(__dirname,'csv'))
-// if(!fs.existsSync(path.join(__dirname,'csv','a')))
-//     fs.mkdirSync(path.join(__dirname,'csv','a'))
-// if(!fs.existsSync(path.join(__dirname,'csv','b')))
-//     fs.mkdirSync(path.join(__dirname,'csv','b'))
-// if(!fs.existsSync(path.join(__dirname,'output')))
-//     fs.mkdirSync(path.join(__dirname,'output'))
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+let target_folder, base_folder
+target_folder = 'csv';
+base_folder = 'base';
 
-readline.question('Input base folder', folder => {
-    base
-});
 
-const a_files = fs.readdirSync(path.join(__dirname, 'csv', 'a'));
-const b_files = fs.readdirSync(path.join(__dirname, 'csv', 'b'));
-
-const c_files = fs.readdirSync(path.join(__dirname, 'csv', 'c'));
-const target_files = fs.readdirSync(path.join(__dirname, 'csv', 'd'));
+const c_files = fs.readdirSync(path.join(__dirname, base_folder));
+const target_files = fs.readdirSync(path.join(__dirname, target_folder));
 
 
 let base_data = [];
@@ -42,7 +26,7 @@ async function loadBaseData(filepath) {
 async function loadTargets() {
     let target_data = [];
     for (const file of target_files) {
-        let data = await parser(path.join(__dirname, 'csv', 'd', file));
+        let data = await parser(path.join(__dirname, target_folder, file));
         d = indexing(data, 'Participant ID');
         target_data.push(d);
     }
@@ -70,16 +54,10 @@ async function merge(base, targets) {
 
     XLSX.utils.book_append_sheet(workbook, worksheet);
     XLSX.writeFile(workbook, "output/test.xlsx", {compression: true});
-    // let flatten = json2csv.parse(result, {objects: false, arrays: true});
-
-    // let lines = flatten.split('\n');
-    // lines.splice(0,1);
-    // let _fp = path.join(__dirname, 'output', `test.csv`.toLocaleLowerCase());
-    // fs.writeFileSync(_fp, lines.join('\n'));
 }
 
 async function start() {
-    base_data = await loadBaseData(path.join(__dirname, 'csv', 'c', c_files[0]));
+    base_data = await loadBaseData(path.join(__dirname, base_folder, c_files[0]));
     target_data = await loadTargets();
     merge(base_data, target_data);
 }
@@ -102,81 +80,3 @@ function indexing(data, index) {
     });
     return result;
 }
-
-
-// console.log(data);
-
-let highestMatchCount = 0;
-
-// if (a_files.length > 0 && b_files.length > 0) {
-//     a_files.forEach((a) => {
-//         let file1 = path.join(__dirname, 'csv', 'a', a)
-
-//         b_files.forEach((b) => {
-//             let file2 = path.join(__dirname, 'csv', 'b', b);
-
-//             async function runMerger() {
-//                 let merged = [];
-//                 let part1 = await parser(file1);
-//                 let part2 = await parser(file2);
-//                 let part1Filename = path.basename(file1, '.csv');
-//                 let part2Filename = path.basename(file2, '.csv');
-
-//                 let part1Keys = part1[0];
-//                 let part2Keys = part2[0];
-//                 console.log(part2);
-
-//                 let part1PrimaryKeyIndex = part1Keys.indexOf('Respondent ID');
-//                 let part2PrimaryKeyIndex = part2Keys.indexOf('Participant ID');
-//                 if(part2PrimaryKeyIndex < 0){
-//                     part2PrimaryKeyIndex = part2Keys.indexOf('Respondent ID');
-//                 }
-
-//                 for(let i=0; i<part1.length; i++){
-//                     if(i < 1){
-//                         /**
-//                          * APPEND COLUMN HEADERS
-//                          */
-//                         merged.push([...part1Keys, ...part2Keys]);
-//                     }else{
-//                         /**
-//                          * LOOP THROUGH PART 1 DATA
-//                          */
-//                         let part1CurrentRow = part1[i];
-//                         let matchCount = 0;
-//                         for(let j=1; j<part2.length; j++){
-//                              /**
-//                              * LOOP THROUGH PART 2 DATA
-//                              */
-//                             let part2CurrentRow = part2[j];
-
-//                             /**
-//                              * CHECK PRIMARY KEYS ARE MATCHED THEN APPEND TO "merged" VARIABLE
-//                              */
-//                             let pk1 = part1CurrentRow[part1PrimaryKeyIndex];
-//                             let pk2 = part2CurrentRow[part2PrimaryKeyIndex];
-
-//                             if(pk1 === pk2 && pk1 !== ""){                
-//                                 matchCount++;
-//                                 merged.push([...part1CurrentRow, ...part2CurrentRow]);
-//                             }
-//                         }
-//                         if(highestMatchCount < matchCount){
-//                             highestMatchCount = matchCount;
-//                         }
-                        
-//                     }
-//                 }
-
-//                 let flattenArray = json2csv.parse(merged, {objects: false, arrays: true});
-
-//                 let lines = flattenArray.split('\n');
-//                 lines.splice(0,1);
-//                 let _fp = path.join(__dirname, 'output', `${part1Filename} - ${part2Filename}.csv`.toLocaleLowerCase());
-//                 fs.writeFileSync(_fp, lines.join('\n'));
-//             }
-
-//             runMerger();
-//         });
-//     });
-// }
